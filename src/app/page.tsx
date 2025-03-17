f6bc30b081daf146b1fcf8afa463abb5e69462be
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-03-17 13:35:26 +0100

    wip refactor home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <main>Hello World</main>;
+  return (
+    <main className="flex items-center h-[100vh]">
+      <div className="flex flex-row justify-evenly w-screen">
+        <div>
+          <h1 className="font-bold text-5xl">
+            Hi, I'm{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-[#EF4444] to-[#5F244f] ">
+              Rafael Muijsert
+            </span>
+          </h1>
+          <h2 className="text-xl">Bruh</h2>
+        </div>
+        <div>Photo</div>
+      </div>
+    </main>
+  );
 }
