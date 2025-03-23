1cea22f4c9cf6be7637e41c38fbbb874566c75ac
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-03-23 14:16:25 +0100

    feat: add simple homepage using shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
+import { FileUser, AtSign, Mail } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex items-center h-[100vh]">
       <div className="flex flex-row justify-evenly w-screen">
         <div>
-          <h1 className="font-bold text-5xl">
-            Hi, I'm{" "}
-            <span className="text-transparent bg-clip-text bg-gradient-to-br from-[#EF4444] to-[#5F244f] ">
-              Rafael Muijsert
-            </span>
+          <div className="flex flex-row"></div>
+          <h1 className="text-5xl">
+            Hi, I'm <strong>Rafael Muijsert</strong>
           </h1>
-          <h2 className="text-xl">Bruh</h2>
+          <h3 className="text-xl">
+            I'm a <strong>software engineer</strong> who loves to build clean
+            and scalable systems.
+          </h3>
+          <div className="mt-4">
+            <Button className="mr-4" asChild>
+              <Link href="mailto:rafael@muijsert.org">
+                <AtSign /> Contact me
+              </Link>
+            </Button>
+            <Button variant={"outline"} asChild>
+              <Link href="/">
+                <FileUser /> Resume
+              </Link>
+            </Button>
+          </div>
         </div>
-        <div>Photo</div>
       </div>
     </main>
   );
